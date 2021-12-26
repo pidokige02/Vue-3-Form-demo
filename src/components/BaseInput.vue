61fa4e4a3f0d4cf75060431837598edfe7cbef14
pidokige02 <pidokige0204@gmail.com>
2021-12-26 22:47:04 +0900

    L2-lesson

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+// comment TODO
+// Add label prop
+// Allow v-modeling ==> bind value and placeholder to the input value via prop.
+    // :value="modelValue" // value can be string and number
+    // :placeholder="label"
+// Bind $attrs
+// 
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input
+    v-bind="$attrs"
+    :value="modelValue"
+    :placeholder="label"
+    @input="$emit('update:modelValue', $event.target.value)"
+    class="field"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
